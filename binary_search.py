bfe8ee98a945343f682dee1c26467a4d754f1889
Torrey Hogan <hogantorrey@gmail.com>
2018-12-06 20:40:55 -0800

    functional tree build and print

--- binary_search.py
+++ binary_search.py
@@ -14,47 +14,45 @@ class Tree:
 		self.head = None
 		self.hash_store = {}
 
-	def build_tree(self):
-		self.head = Node(self.arr[0])
-		node = self.head
-		print(self.head.value)
-		insert_level(node,0)
-
 	def insert_level(self,node,i):
 		if i == 0:
-			node.left = insert_level(node, int(2 * i + 1))
+			node.left = self.insert_level(node, int(2 * i + 1))
 			node.left.parent = node
-			node.right = insert_level(node, int(2 * i + 2))
+			node.right = self.insert_level(node, int(2 * i + 2))
 			node.right.parent = node 
 		elif i < len(self.arr):
 			node = Node(self.arr[i])
-			node.left = insert_level(node, int(2 * i +1))
+			node.left = self.insert_level(node, int(2 * i +1))
 			if node.left != None:
 				node.left.parent = node 
-			node.right = insert_level(node, int(2*i + 2))
+			node.right = self.insert_level(node, int(2*i + 2))
 			if node.right != None:
 				node.right.parent = node 
 		else:
 			return None
-
 		return node 
+
+	def build_tree(self):
+		self.head = Node(self.arr[0])
+		print(self.head.value)
+		self.insert_level(self.head,0)
 
 	def print_tree(self):
 		self.hash_store.setdefault(1,None)
 		self.hash_store[1] = self.head.value
-		# node_print(self.head, 2)
+		self.node_print(self.head, 2)
 
 	def node_print(self,node,level):
 		if node.left == None and node.right == None:
 			return
 		elif node.right == None:
 			self.hash_store.setdefault(level,[])
-			hash_store[level].append(node.left.value)
+			self.hash_store[level].append(node.left.value)
 		else:
 			self.hash_store.setdefault(level,[])
-			self.hash_store.append([node.left.value,node.right.value])
-			node_print(node.left,int(level + 1))
-			node_print(node.right,int(level+1))
+			self.hash_store[level].append([node.left.value,node.right.value])
+			self.node_print(node.left,int(level + 1))
+			self.node_print(node.right,int(level+1))
 
 
 
